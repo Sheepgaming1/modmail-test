2e0b721c641a9a5e0e8df3f328ea0a598c24d362
Sheepgaming1 <93445489+Sheepgaming1@users.noreply.github.com>
2023-01-31 01:01:28 +0000

    Update log.ts

--- api/log.ts
+++ api/log.ts
@@ -26,7 +26,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 			${msg.attachments && msg.attachments.length > 0 ? `
 			<div class="files">
 					${msg.attachments.map((f, v) => `<a href="${f}">Image ${v}</a>`).join(' ')}
-					${msg.timestamp
 			</div>` : ''}
 		</div>
 	</td>
